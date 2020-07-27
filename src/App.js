5a5db81c554fb8d6b7c4c44d545d4e8429792e28
aj709 <55025205+aj709@users.noreply.github.com>
2020-07-27 12:02:40 +0530

    connected to hosted server

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component{
 
   onButtonSubmit = () =>{
     this.setState({imageUrl:this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://serene-bayou-03281.herokuapp.com/imageurl',{
       method : 'post',
       headers : {'Content-Type' : 'application/json'},
       body : JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component{
     .then(response => response.json())
     .then(response => {
       if(response){
-        fetch('http://localhost:3000/images',{
+        fetch('https://serene-bayou-03281.herokuapp.com/images',{
           method : 'put',
           headers : {'Content-Type' : 'application/json'},
           body : JSON.stringify({
